20167f6e2c2a8090a58dfe4e667e1bae64d5fd96
AngusNew <46511259+AngusNew@users.noreply.github.com>
2019-07-07 13:36:30 +0800

    Add files via upload

---
+++ 2017012696YangSheng/db.py
@@ -0,0 +1,10 @@
+import pymongo
+
+client = pymongo.MongoClient('127.0.0.1', 27017)
+mydb = client['ys_db']
+
+filename='gen_data.txt'
+f = open(filename, 'r')
+lists=f.readlines()
+for i in lists:
+    mydb.data.insert({'ys':i})
