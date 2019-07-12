e007430aac828eb6c4e222a296c96a1d2d5e6827
2017011868成宇歌 <48313874+nenucyg@users.noreply.github.com>
2019-07-12 13:12:25 +0800

    Add files via upload

---
+++ GUI.py
@@ -0,0 +1,11 @@
+﻿from tkinter import *
+from random_num import *
+from moveto_db import *
+from matplot_data import *
+b = Tk()
+Button(b, anchor='center', width=30, height=4, text='生成十万条随机数', bg='blue',activebackground='gray', fg='red', borderwidth=5,compound='bottom', font='20px',command=main_1).pack()
+Button(b, anchor='center', width=30, height=4, text='打开生成数据的文本文件', bg='blue',activebackground='gray', fg='red', borderwidth=5,compound='bottom', font='20px',command=read_file).pack()
+Button(b, anchor='center', width=30, height=4, text='生成数据并导入数据库', bg='blue',activebackground='gray', fg='red', borderwidth=5,compound='bottom', font='20px',command=main_2).pack()
+Button(b, anchor='center', width=30, height=4, text='显示数据库中的内容', bg='blue',activebackground='gray', fg='red', borderwidth=5,compound='bottom', font='20px',command=show_data).pack()
+Button(b, anchor='center', width=30, height=4, text='数据可视化', bg='blue',activebackground='gray', fg='red', borderwidth=5,compound='bottom', font='20px',command=main_3).pack()
+b.mainloop()
