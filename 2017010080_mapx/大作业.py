f68b8c40900a7834d97500bd56c43fbfeefcb059
2017010080马佩瑄 <49185543+WenNeptune@users.noreply.github.com>
2019-07-13 16:53:31 +0800

    Add files via upload

---
+++ 2017010080_mapx/大作业.py
@@ -0,0 +1,62 @@
+import string
+import random
+from pymongo import MongoClient
+import pymongo
+import pandas as pd
+from matplotlib import pyplot as plt
+import numpy as np 
+
+
+#1.链接本地数据库服务
+name = MongoClient('localhost')
+#2.链接本地数据库 demo 没有会创建
+db = name.mapx#demo数据库名
+#3.创建，连接集合
+emp = db.pxm #集合名字
+emp.remove(None)
+def generateRandomNumber(i):
+    while i!=0:
+        Int=random.randint(1,100000)
+        Float=random.random()
+        Char=random.choice('ud7hfji8jkgikgjksdjgshjgdsgjgdsjjkdkdgsjkgdsgdjdsghgdsgdjskjdgsjkdgkgd')
+        Str=''.join(random.sample(string.ascii_letters + string.digits,random.randint(1,10)))
+        dict={str(Int):Str}
+        lop=(Int,Float,Char,Str,dict)
+        yield lop
+        randomNumber = {
+            '整型': Int,
+            '浮点数':Float,
+            '字符': Char,
+            '字典': dict,
+            '字符串':Str
+        }
+        emp.insert_one(randomNumber)
+        i= i - 1
+    return 'done'
+g = generateRandomNumber(100000)
+f = open("mpx.txt", "w", encoding="utf-8")
+while True :
+    try:
+        y = next(g)
+        print(y, file = f)
+    except StopIteration as e:
+        print(e.value)
+        break
+f.close()
+
+ emp = db['pxm']
+# 读取数据
+y = data['整型']
+# 选择需要统计的字段
+
+
+
+fig = plt.figure(figsize=(5,5))
+ax = fig.add_subplot(111)
+x=np.arange(1,100001) 
+ax.scatter(x,y,c='green')
+ax.legend('int') 
+ax.grid(True)
+plt.show()
+
+
---
+++ 2017010080_mapx/散点图的创建.py
@@ -0,0 +1,24 @@
+import pymongo
+import pandas as pd
+from matplotlib import pyplot as plt
+import numpy as np 
+import random
+
+# 连接数据库
+client = pymongo.MongoClient('localhost', 27017)
+db = client['mapx']
+table = db['pxm']
+# 读取数据
+data = pd.DataFrame(list(table.find()))
+# 选择需要统计的字段
+y = data['整型']
+
+
+
+fig = plt.figure(figsize=(5,5))
+ax = fig.add_subplot(111)
+x=np.arange(1,100001) 
+ax.scatter(x,y,c='green')
+ax.legend('int') 
+ax.grid(True)
+plt.show()
