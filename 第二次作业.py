95f7e810218f90a52005594f7bb5863faf4fd671
2017010070周冠男 <38697704+zhougn576@users.noreply.github.com>
2019-07-13 20:45:49 +0800

    Add files via upload

--- 第二次作业.py
+++ 第二次作业.py
@@ -3,42 +3,42 @@ import random
 from pymongo import MongoClient
 
 #1.链接本地数据库服务
-name = MongoClient('localhost')
 #2.链接本地数据库 demo 没有会创建
-db = name.random_number   #demo数据库名
 #3.创建，连接集合
-emp = db.myrandom  #集合名字
+name = MongoClient('localhost'，27017)
+db = name.zhougn   #demo数据库名
+emp = db.zhougn576  #集合名字
 emp.remove(None)
 
-def Random(k):
-    c=0
-    while k!=0:
-        c+=1
-        wuli=random.randint(1,10)
-        ran_str =  ''.join(random.sample(string.ascii_letters + string.digits,wuli))
-        leo=random.uniform(1,10)
-        m=random.randint(1,100000)
-        n=random.choice(string.ascii_letters)
-        dicta={n:leo}
-        dictb={n:m}
-        dictc={n:ran_str}
-        c1=(str(c)+':')
-        tup1 = (c1,ran_str,leo,m,dicta,dictb,dictc)
+def Random(i):
+    cout=0
+    while i!=0:
+        cout+=1
+        num=random.randint(1,10)
+        ran_str =  ''.join(random.sample(string.ascii_letters + string.digits,num))
+        unif=random.uniform(1,100)
+        rand=random.randint(1,1000)
+        key=random.choice(string.ascii_letters)
+        dict1={key:unif}
+        dict2={key:rand}
+        dict3={key:ran_str}
+        cout1=(str(cout)+':')
+        tup1 = (cout1,ran_str,unif,rand,dict1,dict2,dict3)
         yield tup1
         randomNumber = {
-            '序号':c,
-            '整型': m,
-            '浮点数':leo,
+            '序号':cout,
+            '整型': rand,
+            '浮点数':unif,
             '字符': ran_str,
-            '字典1': dicta,
-            '字典2':dictb,
-            '字典3':dictc
+            '字典1': dict1,
+            '字典2':dict2,
+            '字典3':dict3
         }
         emp.insert_one(randomNumber)
-        k= k - 1
+        i= i - 1
     return 'done'
 g = Random(100000)
-f = open("out.txt", "w", encoding="utf-8")
+f = open("db.txt", "w", encoding="utf-8")
 while True :
     try:
         y = next(g)
@@ -47,8 +47,3 @@ while True :
         print(e.value)
         break
 f.close()
-
-#f1=open("数据库.txt","w")
-#for k in emp.find():
- #   print(k,file=f1)
-#f1.close()
