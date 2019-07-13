a973824cdd6d9f516fade0845a2b70e34f246ded
nixror <48321792+nixror@users.noreply.github.com>
2019-07-14 00:23:03 +0800

    Update graphshow.py

--- 2017013519_HuangXingshuo/graphshow.py
+++ 2017013519_HuangXingshuo/graphshow.py
@@ -1,20 +1,23 @@
 from dataGenerate import *
-import matplotlib.pyplot as plt
-
 #对于随机正整数，画出对17取模后得到的各个数的数量折线图
-
+import matplotlib.pyplot as plt
 class IntGenerate(DataGenerate):    #IntGenerate 是 DataGenerate的子类,生成随机整数的绝对值对17取模的结果
     def intgenerate(self):
         for i in range(100000):
             yield abs(self.datagenerateIntFloatStr(1)) % 17   #生成整数
-            
-sampleOfIntGenerate = IntGenerate()
-intResults = sampleOfIntGenerate.intgenerate()
-res = []
-for i in range(17):
-    res.append(0)
-for intResult in intResults:
-    res[intResult] += 1
-plt.plot(res)
-plt.show()
+class Plot:
+    def __init__(self):
+        self.sampleOfIntGenerate = IntGenerate()
+        self.intResults = self.sampleOfIntGenerate.intgenerate()
+        self.res = []
+        for i in range(17):
+            self.res.append(0)
+    def plotGraph(self):
+        for intResult in self.intResults:
+            self.res[intResult] += 1
+        plt.plot(self.res)
+        plt.show()
 
+if __name__ == '__main__':
+    pt = Plot()
+    pt.plotGraph()
