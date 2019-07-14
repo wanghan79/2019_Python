45f9bcc0e93420565f93b5c877f51a693aafd62d
2017013510王新然 <48324860+wangxr99@users.noreply.github.com>
2019-07-14 14:19:34 +0800

    Add files via upload

---
+++ 2017013510王新然/CreateDb.py
@@ -0,0 +1,20 @@
+﻿import pymongo
+from randomGen import Generator
+
+#连接数据库
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+mydb = myclient["database"]
+mycol = mydb["RandomNumbers"]
+
+x =mycol.delete_many({})
+i=0
+for n in Generator.dataGenerate():
+    i=i+1
+    mydict={'_id':i,'content':n}
+    x=mycol.insert_one(mydict)
+
+
+file = open('database.txt', 'w')
+for x in mycol.find():
+    file.write(str(x) + '\n')
+
