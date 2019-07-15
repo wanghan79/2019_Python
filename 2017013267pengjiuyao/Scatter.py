292a6a35696a198ef0743ea076d65506dcaa35df
2017013267  pengjiuyao <48319953+pengjiuyao@users.noreply.github.com>
2019-07-15 10:21:51 +0800

    Add files via upload

---
+++ 2017013267pengjiuyao/Scatter.py
@@ -0,0 +1,13 @@
+import pymongo
+import matplotlib.pyplot as plt
+import numpy as np
+
+def scatt(): 
+ myclient=pymongo.MongoClient("mongodb://localhost:27017")
+ mydb=myclient["runoobdb"]
+ mycol=mydb["sites"]
+
+ x=np.arange(1,100001)
+ y=np.loadtxt('data.txt',delimiter=',',usecols=1,unpack=False)
+ plt.scatter(x,y,c = 'r',marker='o',s=1,alpha=0.5)
+ plt.show()
