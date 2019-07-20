f856409af92af3990773966d937d58d9d1cade04
2017010062王玉虎 <49155366+wangyhu@users.noreply.github.com>
2019-07-20 17:50:03 +0800

    Add files via upload

---
+++ 2017010062_Wangyuhu/2017010062 data mongodb.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+class DataStorage:
+
+    """Store the data to mongodb"""
+
+
+
+    @staticmethod
+
+    def random_data_storage(mysites, filename):  # 存储size条随机数据
+
+        with open(filename) as file_object:
+
+            for line in file_object:
+
+                mysites.insert_one(eval(line))
